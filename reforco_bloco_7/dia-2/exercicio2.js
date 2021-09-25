94c09a0b32520ae43d68fa5b12a0a8a0eb679590
jpcp0614 <jpcp0614@gmail.com>
2021-09-25 17:09:43 -0300

    exercicio1 - 2)

--- reforco_bloco_7/dia-2/exercicio2.js
+++ reforco_bloco_7/dia-2/exercicio2.js
@@ -21,11 +21,14 @@ const lesson3 = {
 // * 1)
 // * Crie uma função para adicionar o turno da noite na lesson2.
 // * Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
-
 const addNewKey = (obj, key, value) => obj[key] = value;
 addNewKey(lesson2, 'turno', 'manhã');
 console.log(lesson2);
 
+
 // * 2
 // * Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const arrayKeys = obj => Object.keys(obj);
+console.log(arrayKeys(lesson1));
+
 
