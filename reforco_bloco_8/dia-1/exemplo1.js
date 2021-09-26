d4aca570ae22fd20b8dc36f9774502f5886aaf0a
jpcp0614 <jpcp0614@gmail.com>
2021-09-26 19:05:05 -0300

    exemplo1

--- reforco_bloco_8/dia-1/exemplo1.js
+++ reforco_bloco_8/dia-1/exemplo1.js
@@ -1,15 +1,15 @@
-// 1 - Crie uma função que retorne a string 'Acordando!!'
+// * 1 - Crie uma função que retorne a string 'Acordando!!'
 const wakeUp = () => 'Acordando!!';
 
-// 2 - Crie outra função que retorne a string 'Bora tomar café!!'
+// * 2 - Crie outra função que retorne a string 'Bora tomar café!!'
 const breakfast = () => 'Bora tomar café!!';
 
-// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!'
+// * 3 - Crie mais uma função que retorne a string 'Partiu dormir!!'
 const sleep = () => 'Partiu dormir!!';
 
-// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta
-// função para que imprima no console o resultado da execução das funções
-// que você criou nos exemplos anteriores.
+// * 4 - Agora desenvolva uma HOF chamada doingThings e configure esta
+// * função para que imprima no console o resultado da execução das funções
+// * que você criou nos exemplos anteriores.
 const doingThings = (callback) => console.log(callback);
 doingThings(wakeUp());
 doingThings(breakfast());
