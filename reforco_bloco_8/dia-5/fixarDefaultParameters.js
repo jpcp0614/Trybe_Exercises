9ae3ae9ee95b860d441d394999e06ffc62093305
jpcp0614 <jpcp0614@gmail.com>
2021-09-30 17:30:58 -0300

    fixar - defaultParameters

---
+++ reforco_bloco_8/dia-5/fixarDefaultParameters.js
@@ -0,0 +1,6 @@
+//* Escreva uma função multiply que multiplique dois números passados como argumentos.
+//* Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
+const multiply = (number = 1, value = 1) => number * value;
+console.log(multiply(8, 2));
+console.log(multiply(3));
+console.log(multiply());
