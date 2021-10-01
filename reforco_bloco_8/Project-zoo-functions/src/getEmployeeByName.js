a6d3f87273509fbd50f8ce5b1c42f38bdf2d8f5e
jpcp0614 <jpcp0614@gmail.com>
2021-10-01 12:16:21 -0300

    getEmployeeByName OK

---
+++ reforco_bloco_8/Project-zoo-functions/src/getEmployeeByName.js
@@ -0,0 +1,10 @@
+const data = require('../data/zoo_data');
+
+function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) return {};
+  return data.employees
+    .find(employee =>
+      employee.firstName === employeeName || employee.lastName === employeeName);
+};
+
+module.exports = getEmployeeByName;
