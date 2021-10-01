938cb5c11553a9355d1af78d750bbbd3664c444b
jpcp0614 <jpcp0614@gmail.com>
2021-09-30 22:05:41 -0300

    test

--- reforco_bloco_7/Project-js-unit-tests/tests/creatStudent.test.js
+++ reforco_bloco_7/Project-js-unit-tests/tests/creatStudent.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const createStudent = require('../src/createStudent');
+const createStudent = require('../src/creatStudent');
 
 /*
   Dada uma função chamada createStudent que recebe como parâmetro um nome, retorne um objeto que contenha duas chaves:
